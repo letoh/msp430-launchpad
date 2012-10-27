c121f5dde2d4cf547bfd31c8b054eddeed46cbe5
letoh <letoh.tw@gmail.com>
2012-10-28 03:01:22 +0800

    add adc10 experiment (led sensing)

---
+++ 0005-adc/main.c
@@ -0,0 +1,42 @@
+#include <msp430g2553.h>
+
+unsigned int analogRead(unsigned int ch)
+{
+	ADC10CTL1 = ADC10SSEL_0 + ch;
+	ADC10CTL0 |= ADC10SC;
+
+	while (1)
+	{
+		if ((ADC10CTL1 ^ ADC10BUSY) && (ADC10CTL0 & ADC10IFG))
+			break;
+	}
+	ADC10CTL0 &= ~ADC10IFG;
+	return ADC10MEM;
+}
+
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;
+	/* setup adc10 */
+	ADC10CTL0 = ADC10ON + ADC10SHT_2 + SREF_1 + REFON + REF2_5V;
+	ADC10CTL0 |= ENC;
+
+	P1DIR |= BIT6;
+
+	unsigned int i, adcval;
+	for(;;)
+	{
+		for(adcval = 0, i = 0; i < 8; ++i)
+		{
+			adcval += analogRead(INCH_0);
+			__delay_cycles(150);
+		}
+		adcval >>= 3;
+		if(adcval < 500)
+			P1OUT |= BIT6;
+		else
+			P1OUT &= ~BIT6;
+		__delay_cycles(100000);
+	}
+}
+
