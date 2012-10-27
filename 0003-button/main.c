198599495db243b1bff21c13f3b1c32839efe041
letoh <letoh.tw@gmail.com>
2012-10-27 18:06:41 +0800

    add button experiment

---
+++ 0003-button/main.c
@@ -0,0 +1,34 @@
+#include <msp430g2553.h>
+
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;
+
+	/* setup button */
+	P1OUT |= BIT3;
+	P1REN |= BIT3;  /* setup pull-up resistor */
+
+	unsigned char state = P1IN & BIT3;
+
+	P1DIR |= BIT0 | BIT6;
+	P1OUT &= ~(BIT0 | BIT6);
+	P1OUT |= ~((state) >> 3);
+
+	unsigned short cycle = 0;
+
+	for(;;)
+	{
+		if(state != (P1IN & BIT3))
+		{
+			state = P1IN & BIT3;
+			P1OUT ^= BIT0;
+		}
+		if(++cycle >= 1000)
+		{
+			P1OUT ^= BIT6;
+			cycle = 0;
+		}
+		__delay_cycles(1000);
+	}
+}
+
