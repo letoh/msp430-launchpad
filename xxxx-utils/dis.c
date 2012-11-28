054ae844b0e12fc537f5cde6291afdb3595326ee
letoh <letoh.tw@gmail.com>
2012-11-28 14:52:15 +0800

    implement simple disassembler

---
+++ xxxx-utils/dis.c
@@ -0,0 +1,271 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned short word_t;
+
+typedef struct _disCtxt_t disCtxt_t;
+struct _disCtxt_t
+{
+	word_t addr;
+	word_t instr;
+	const word_t *cur;
+	const word_t *end;
+	unsigned char *buf;
+	long length;
+};
+
+#define isFmt1(instr)  (((instr) & 0xfc00) == 0x1000)
+#define isFmt2(instr)  (((instr) & 0xe000) == 0x2000)
+#define isFmt3(instr)  (((instr) & 0xf000) >= 0x4000)
+
+#define fetch(pCtxt)             do{\
+	(pCtxt)->instr = *(pCtxt)->cur;\
+	++(pCtxt)->cur, (pCtxt)->buf += sizeof(word_t), (pCtxt)->addr += sizeof(word_t);\
+	} while(0);
+#define decode(instr,pos,mask)  (((instr) >> (pos)) & (mask))
+// ((offset & 0x200)?-((~offset & 0x3ff) + 1):offset)
+#define evalsign(val,bit)       (((val) & (1 << ((bit) - 1)))?-((~(val) & ((1 << (bit)) - 1)) + 1):(val))
+
+int dis_operand(disCtxt_t *pCtxt, word_t reg, word_t mode)
+{
+	switch(reg)
+	{
+	case 0:
+		switch(mode)
+		{
+		case 0: case 2:
+			printf("r%d", reg);
+			break;
+		case 1:
+			fetch(pCtxt);
+			printf("0x%04x(r0)", pCtxt->instr);
+			break;
+		case 3:  // only for As
+			fetch(pCtxt);
+			printf("#%d", evalsign(pCtxt->instr,16));
+			break;
+		default: return 1;
+		}
+		break;
+	case 2:
+		switch(mode)
+		{
+		case 0:
+			printf("r%d", reg);
+			break;
+		case 1:
+			fetch(pCtxt);
+			printf("&0x%04x", pCtxt->instr);
+			break;
+		case 2: case 3:
+			printf("#%d", 1 << mode);
+			break;
+		default: return 1;
+		}
+		break;
+	case 3:
+		switch(mode)
+		{
+		case 3:
+#if 0
+			printf("#-1");
+			break;
+		case 0: case 1: case 2:
+			printf("#%d", mode);
+#else
+		case 0: case 1: case 2:
+			printf("#%d", evalsign(mode, 2));
+#endif
+			break;
+		default: return 1;
+		}
+		break;
+	default:
+		if(reg > 15) return 1;
+		switch(mode)
+		{
+		case 0:
+			printf("r%d", reg);
+			break;
+		case 1:
+			fetch(pCtxt);
+			printf("0x%04x(r%d)", pCtxt->instr, reg);
+			break;
+		case 2:
+			printf("@r%d", reg);
+			break;
+		case 3:
+			printf("@r%d+", reg);
+			break;
+		default: return 1;
+		}
+		break;
+	}
+	return 0;
+}
+
+const char* fmt1_instr_str[] =
+{
+	"rrc", "swpb", "rra", "sxt", "push", "call", "reti"
+};
+int dis_fmt1(disCtxt_t *pCtxt)
+{
+	word_t addr = pCtxt->addr - sizeof(word_t);
+	word_t opcode = decode(pCtxt->instr, 7, 0x7);
+	word_t byteop = decode(pCtxt->instr, 6, 0x1);
+	word_t admode = decode(pCtxt->instr, 4, 0x3);
+	word_t dstreg = decode(pCtxt->instr, 0, 0xf);
+	if(opcode > 6) return 1;
+	if(byteop && ((opcode & 0x1) || opcode == 6)) return 1;
+	printf("[%04x] %02x %02x | %s%s",
+			addr, pCtxt->instr & 0xff, pCtxt->instr >> 8,
+			fmt1_instr_str[opcode], byteop?".b":"");
+	if(opcode < 6)
+	{
+		printf("  ");
+		dis_operand(pCtxt, dstreg, admode);
+		if(dstreg == 0 && admode == 3)
+			printf("   ; 0x%04x", pCtxt->instr);
+	}
+	printf("\n");
+	return 0;
+}
+
+const char* fmt2_instr_str[] =
+{
+	"jnz", "jz", "jnc", "jc", "jn", "jge", "jl", "jmp"
+};
+int dis_fmt2(disCtxt_t *pCtxt)
+{
+	word_t addr   = pCtxt->addr - sizeof(word_t);
+	word_t cond   = decode(pCtxt->instr, 10, 0x7);
+	word_t offset = decode(pCtxt->instr, 0, 0x3ff);
+	//int value = 2 * (((offset & 0x200)?-((~offset & 0x3ff) + 1):offset) + 1);
+	int value = 2 * (evalsign(offset,10) + 1);
+	printf("[%04x] %02x %02x | %-4s $%+d ; abs(0x%04x)\n",
+			addr, pCtxt->instr & 0xff, pCtxt->instr >> 8,
+			fmt2_instr_str[cond], value, addr + value);
+	return 0;
+}
+
+const char* fmt3_instr_str[] =
+{
+	NULL, NULL, NULL, NULL,
+	"mov", "add", "addc", "subc", "sub", "cmp", "dadd", "bit", "bic", "bis", "xor", "and"
+};
+int dis_fmt3(disCtxt_t *pCtxt)
+{
+	word_t addr = pCtxt->addr - sizeof(word_t);
+	word_t opcode = decode(pCtxt->instr, 12, 0xf);
+	word_t srcreg = decode(pCtxt->instr, 8, 0xf);
+	word_t admode = decode(pCtxt->instr, 7, 0x1);
+	word_t byteop = decode(pCtxt->instr, 6, 0x1);
+	word_t asmode = decode(pCtxt->instr, 4, 0x3);
+	word_t dstreg = decode(pCtxt->instr, 0, 0xf);
+
+	printf("[%04x] %02x %02x | %s%s  ",
+			addr, pCtxt->instr & 0xff, pCtxt->instr >> 8,
+			fmt3_instr_str[opcode], byteop?".b":"");
+
+	dis_operand(pCtxt, srcreg, asmode);
+	printf(", ");
+	dis_operand(pCtxt, dstreg, admode);
+	printf("\n");
+	return 0;
+}
+
+int dis_data(disCtxt_t *pCtxt)
+{
+	word_t addr = pCtxt->addr - sizeof(word_t);
+	printf("[%04x] %02x %02x | .word 0x%04x\n",
+			addr, pCtxt->instr & 0xff, pCtxt->instr >> 8,
+			pCtxt->instr);
+	return 0;
+}
+
+int dis_pseudo(disCtxt_t *pCtxt)
+{
+	word_t addr = pCtxt->addr - sizeof(word_t);
+	switch(pCtxt->instr)
+	{
+	case 0x4303: // mov #0, #0 => nop
+		printf("[%04x] %02x %02x | nop\n", addr, pCtxt->instr & 0xff, pCtxt->instr >> 8);
+		return 0;
+	case 0xc232: // bic #8,sr => dint
+		printf("[%04x] %02x %02x | dint\n", addr, pCtxt->instr & 0xff, pCtxt->instr >> 8);
+		return 0;
+	case 0xd232: // bis #8,sr => eint
+		printf("[%04x] %02x %02x | eint\n", addr, pCtxt->instr & 0xff, pCtxt->instr >> 8);
+		return 0;
+	case 0xc312: // bic #1,sr => clrc
+		printf("[%04x] %02x %02x | clrc\n", addr, pCtxt->instr & 0xff, pCtxt->instr >> 8);
+		return 0;
+	case 0xc222: // bic #4,sr => clrn
+		printf("[%04x] %02x %02x | clrn\n", addr, pCtxt->instr & 0xff, pCtxt->instr >> 8);
+		return 0;
+	case 0xc322: // bic #2,sr => clrz
+		printf("[%04x] %02x %02x | clrz\n", addr, pCtxt->instr & 0xff, pCtxt->instr >> 8);
+		return 0;
+	case 0xd312: // bis #1,sr => setc
+		printf("[%04x] %02x %02x | setc\n", addr, pCtxt->instr & 0xff, pCtxt->instr >> 8);
+		return 0;
+	case 0xd222: // bis #4,sr => setn
+		printf("[%04x] %02x %02x | setn\n", addr, pCtxt->instr & 0xff, pCtxt->instr >> 8);
+		return 0;
+	case 0xd322: // bis #2,sr => setz
+		printf("[%04x] %02x %02x | setz\n", addr, pCtxt->instr & 0xff, pCtxt->instr >> 8);
+		return 0;
+	case 0x4130: // mov @sp+,pc
+		printf("[%04x] %02x %02x | ret\n", addr, pCtxt->instr & 0xff, pCtxt->instr >> 8);
+		return 0;
+	}
+	return 1;
+}
+
+void dis(disCtxt_t *pCtxt)
+{
+	while(pCtxt->cur < pCtxt->end)
+	{
+		fetch(pCtxt);
+		if(pCtxt->instr == 0) {} else
+		if(0 == dis_pseudo(pCtxt)) { continue; } else
+		if(isFmt1(pCtxt->instr)) { dis_fmt1(pCtxt); } else
+		if(isFmt2(pCtxt->instr)) { dis_fmt2(pCtxt); } else
+		if(isFmt3(pCtxt->instr)) { dis_fmt3(pCtxt); } else
+		{ dis_data(pCtxt); }
+	}
+}
+
+int dis_file(const char *fn)
+{
+	FILE *fp = fopen(fn, "rb");
+	unsigned char *buf;
+	disCtxt_t ctxt;
+	if(NULL == fp) return 1;
+
+	fseek(fp, 0, SEEK_END);
+	ctxt.length = ftell(fp);
+	rewind(fp);
+	//printf("length: %d\n", ctxt.length);
+	buf = (unsigned char *)malloc(ctxt.length);
+	if(NULL == buf) { fclose(fp); return 1; }
+
+	ctxt.buf = buf;
+	ctxt.addr = 0xc000;
+	ctxt.cur = (const word_t *) ctxt.buf;
+	ctxt.end = ctxt.cur + (ctxt.length / 2);
+	if(fread(ctxt.buf, ctxt.length, 1, fp))
+		dis(&ctxt);
+
+	free(buf);
+	fclose(fp);
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	if(argc < 2) { fprintf(stderr, "Usage: %s <bin>\n", argv[0]); return -1; }
+
+	return dis_file(argv[1]);
+}
+
